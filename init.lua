7d226171bdf0e1ce4eab69af5255c981182f5ba0
Shusen <19257104+shusen73@users.noreply.github.com>
2025-02-16 16:28:41 +0900

    fix confilct

--- init.lua
+++ init.lua
@@ -704,6 +704,7 @@ require('lazy').setup({
         templ = {
           filetypes = { 'templ', 'html' },
         },
+        html = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
